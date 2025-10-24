74dfa64649d6f6616a921596f23dd9d33ccc3c96
owlcatraz <143764220+owlcatraz3@users.noreply.github.com>
2025-10-24 17:22:43 +1100

    Added comments in killconnection.sh
    Added comments to clarify the purpose of the script.

--- killconnection.sh
+++ killconnection.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# Get the pid of the websocket connection from a source (if reverse proxy is used to direct victim to the malicious webpage), then kill the connection. Edit the source IP address as needed.
 pid=$(sudo docker exec evilnovnc sudo ss -tunp | grep 192.168.100.22 | awk -F'pid=' '{print $2}' | awk -F',' '{print $1}')
 
 sudo docker exec evilnovnc sudo kill $pid
